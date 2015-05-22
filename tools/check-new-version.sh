5dbc29af7da58fd0aa0b4da1903960024e26a40d
Timothy Gu <timothygu99@gmail.com>
2015-05-22 15:29:44 -0700

    check-new-version: output JSON

--- tools/check-new-version.sh
+++ tools/check-new-version.sh
@@ -5,5 +5,15 @@ stable=`curl -s 'http://lilypond.org/unix.html' | \
 unstable=`curl -s 'http://lilypond.org/development.html' | \
 	sed -n 's,.*http://download\.linuxaudio\.org/lilypond/binaries/linux-64/lilypond-\([0-9\.-]*\)\.linux-64\.sh".*,\1,p'`
 
-echo Stable: $stable
-echo Unstable: $unstable
+stable_src=`echo $stable | cut -d'-' -f1`
+unstable_src=`echo $unstable | cut -d'-' -f1`
+
+cat <<EOF >version.json
+{
+  "stable": "$stable_src"
+, "unstable": "$unstable_src"
+}
+EOF
+
+echo stable $stable
+echo unstable $unstable
