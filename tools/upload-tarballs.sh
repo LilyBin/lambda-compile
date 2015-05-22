023f83a03728451a327474c33ba0a40d4a7928c2
Timothy Gu <timothygu99@gmail.com>
2015-05-22 15:49:14 -0700

    Fix upload-tarballs.sh

--- tools/upload-tarballs.sh
+++ tools/upload-tarballs.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 for f in lilypond-*.tar; do
-	aws s3 cp --storage-class REDUCED_REDUNDANCY $f s3://lilypad-tarball/
+	aws --profile admin s3 cp --storage-class REDUCED_REDUNDANCY $f s3://lilypad-tarball/
 done
+[ -e version.json ] && aws --profile admin s3 cp --storage-class REDUCED_REDUNDANCY version.json s3://lilypad-tarball/
